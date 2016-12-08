717f26a8796a2e7f57b39db638d6d0942c27f510
Aaron Trank <aarontrank@gmail.com>
2016-12-08 13:35:00 -0800

    pull blacklist updates via npm run blacklist:update

---
+++ blacklist/dedupe.js
@@ -0,0 +1,25 @@
+//helper function to properly format blacklist 
+var mongoose = require('mongoose');
+var fs = require('fs');
+mongoose.connect('mongodb://localhost/newsgate');
+var News = require('../server/models/newsModel.js');
+var deduped = {};
+fs.readFile('./blacklist/blacklist.js', function(err, data){
+  if (err) {
+    console.log('Error reading from blacklist file: ' + err);
+    process.exit();
+  } else {
+    var blacklist = JSON.parse(data);
+    var newsToAdd = blacklist.length;
+    var newsAdded = 0;
+    blacklist.forEach(function(badSite){
+      deduped[badSite.url] = badSite.rating;
+      badSite.rating.type = 'fake';
+    });
+    fs.writeFile('blacklistNew.js', JSON.stringify(deduped), (err) => {
+      if (err) throw err;
+      console.log('It\'s saved!');
+      process.exit();
+    });
+  }
+});
