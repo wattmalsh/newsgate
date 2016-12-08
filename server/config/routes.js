93739ed1324daa201dceb6bf7321dce00629ebbf
Emerson Hum <emersonhum@gmail.com>
2016-12-08 12:35:44 -0800

    Connect client with watson title extraction

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,6 +1,10 @@
 var newsController = require('../controllers/newsController.js');
 var watsonController = require('../watson/watsonController.js');
+const googleTrends = require('../trends/googleTrends');
+const twitterSearch = require('../trends/twitterTrends');
 
 module.exports = function (app, express) {
   app.post('/api', newsController.isFakeNews);
+  app.get('/api/googleTrends', googleTrends.getGoogleTrends);
+  app.get('/twitter', twitterSearch.getTweetsOnTopic);
 };
