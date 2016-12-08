49b84bafcfe7907f2ca5ba866562d8a2825565fb
Aaron Trank <aarontrank@gmail.com>
2016-12-07 16:18:03 -0800

    Stub out route and isFakeNews Controller

--- server/controllers/newsController.js
+++ server/controllers/newsController.js
@@ -3,6 +3,11 @@ var testData = require('../test/loadTestData.js');
 
 
 module.exports = {
-
-
+  isFakeNews: (req, res, next) {
+    // get url off req.params
+    // get domain off url
+    // check db for domain
+      // if domain in db : return "true"
+      // else return "false"
+  }
 }
