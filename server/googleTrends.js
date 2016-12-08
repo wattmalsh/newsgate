8428b6f3dbfdb3fb6159ab47d717636dbafb7968
Emerson Hum <emersonhum@gmail.com>
2016-12-07 16:28:22 -0800

    Create functioning googleTrends API request with cookie work around

---
+++ server/googleTrends.js
@@ -0,0 +1,23 @@
+var googleTrends = require('google-trends-api');
+//API documentation: https://www.npmjs.com/package/google-trends-api
+var bodyParser = require('body-parser');
+
+
+exports.getGoogleTrends = function(request, response) {
+  console.log('got request');
+  // var keywords = request.body.keywords //array of words
+  var keywords = 'trump';
+  var timePeriod = {
+    // date: '201601', //YYYYMM starting date for how far back to retrieve data
+    type: 'day',   //resolution of returned data
+    value: 5       //need to figure out what this is...
+  }
+  googleTrends.trendData({keywords: 'donald trump', timePeriod: timePeriod})
+  .then(function(results) {
+    console.log(results);
+    response.send(results);
+  })
+  .catch(function(err) {
+    console.error('Error with google trends get request', err);
+  });
+};
