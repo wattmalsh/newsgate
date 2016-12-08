2ffdc3e6cf3bf2a714fd1dc69c14e60178d12e6c
FMurray <FMurray@gmail.com>
2016-12-07 17:19:09 -0800

    handle url in db by POSTing a url

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,5 +1,5 @@
 var newsController = require('../controllers/newsController.js');
 
 module.exports = function (app, express) {
-  app.get('/api/:url', newsController.isFakeNews);
+  app.post('/api', newsController.isFakeNews);
 };
