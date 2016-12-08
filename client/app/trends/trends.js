0bf473d9916534844ba4f62db6ed6546a1234ff2
davidank <dandahmann@gmail.com>
2016-12-08 15:54:33 -0800

    Add area under curve for graph

--- client/app/trends/trends.js
+++ client/app/trends/trends.js
@@ -1,32 +1,36 @@
 angular.module('newsgate.trends', [])
-.controller('TrendsController', function($scope) {
-
+.controller('TrendsController', function($scope, Data) {
+  console.log('yoyo before', Data);
+  $scope.data = Data.process(Data.test2);
+  console.log('yoyo after', Data);
 })
-.directive('trendGraph', function(Trends) {
+.directive('trendGraph', function(Data) {
   console.log('isD3 Loaded?', d3);
   return {
     restrict: 'E',
     link: function(scope, element) {
 
+      // define the space that holds the graph
       var margin = {top: 20, right: 20, bottom: 30, left: 50},
           width = 960 - margin.left - margin.right,
           height = 500 - margin.top - margin.bottom;
 
-      // parse the date / time
-      var parseTime = d3.timeParse("%d-%b-%y");
-
-      // set the ranges
+      // define ranges for axis
       var x = d3.scaleTime().range([0, width]);
       var y = d3.scaleLinear().range([height, 0]);
+
+      // define area under the line that was drawn
+      var area = d3.area()
+        .x(function(d) { return x(d.date); })
+        .y0(height)
+        .y1(function(d) { return y(d.value); });
 
       // define the line
       var valueline = d3.line()
           .x(function(d) { return x(d.date); })
-          .y(function(d) { return y(d['value']); });
+          .y(function(d) { return y(d.value); });
 
-      // append the svg obgect to the body of the page
-      // appends a 'group' element to 'svg'
-      // moves the 'group' element to the top left margin
+      // find the custom tag and append svg and attributes
       var svg = d3.select(element[0]).append("svg")
           .attr("width", width + margin.left + margin.right)
           .attr("height", height + margin.top + margin.bottom)
@@ -34,41 +38,35 @@ angular.module('newsgate.trends', [])
           .attr("transform",
                 "translate(" + margin.left + "," + margin.top + ")");
 
-      // Get the data
-      d3.csv("/app/test/data.csv", function(error, data) {
-        if (error) throw error;
-        console.log('original data', data);
-        // format the data
-        data.forEach(function(d) {
-            d.date = parseTime(d.date);
-            d.close = +d.close;
-            // console.log('d.date:', d.date);
-            // console.log('d.close:', d.close);
-        });
-        console.log('original data after formating', data);
-        data = Trends.getTestData();
-        console.log(data);
+      // HardCoded data input
+      // data = Data.process();
+      data = scope.data;
+      console.log(data);
 
-        // Scale the range of the data
-        x.domain(d3.extent(data, function(d) { return d.date; }));
-        y.domain([0, d3.max(data, function(d) { return d['value']; })]);
+      // scale the range of the data
+      x.domain(d3.extent(data, function(d) { return d.date; }));
+      y.domain([0, d3.max(data, function(d) { return d.value; })]);
 
-        // Add the valueline path.
-        svg.append("path")
-            .data([data])
-            .attr("class", "line")
-            .attr("d", valueline);
+      // draw area under curve
+      svg.append("path")
+        .datum(data)
+        .attr("class", "area")
+        .attr("d", area);
 
-        // Add the X Axis
-        svg.append("g")
-            .attr("transform", "translate(0," + height + ")")
-            .call(d3.axisBottom(x));
+      // draw the valueline path
+      svg.append("path")
+          .data([data])
+          .attr("class", "line")
+          .attr("d", valueline);
 
-        // Add the Y Axis
-        svg.append("g")
-            .call(d3.axisLeft(y));
+      // draw the x Axis
+      svg.append("g")
+          .attr("transform", "translate(0," + height + ")")
+          .call(d3.axisBottom(x).ticks(d3.timeDay, 1).tickFormat(d3.timeFormat("%b %d")));
 
-      });
+      // draw the y Axis
+      svg.append("g")
+          .call(d3.axisLeft(y).ticks(5));
 
     }
   };
