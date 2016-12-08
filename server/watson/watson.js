3706c0f5a05dd95597667344edf96b5ab40e00a7
Emerson Hum <emersonhum@gmail.com>
2016-12-07 21:55:29 -0800

    Implement twitter tweets search and add dummy data for client :bird:

--- server/watson/watson.js
+++ server/watson/watson.js
@@ -2,10 +2,11 @@ var AlchemyAPI = require('./alchemyapi');
 var Promise = require('bluebird');
 var alchemyapi = new AlchemyAPI();
 
+var demo_url = 'http://www.clickhole.com/article/ladies-and-gentlemen-please-log-your-computers-dra-5211';
+
 exports.watsonTitle = function(req, res, output) {
   alchemyapi.title('url', demo_url, {}, function(response) {
     output['title'] = { url:demo_url, response:JSON.stringify(response,null,4), results:response };
-    relations(req, res, output);
   });
 };
 
