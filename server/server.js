5a073a94a1905d8fea7c629cd640480727386424
Emerson Hum <emersonhum@gmail.com>
2016-12-07 16:28:22 -0800

    Create functioning googleTrends API request with cookie work around

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const port = 8000;
 const app = express();
+const googleTrends = require('./googleTrends');
 
 // connect to mongo database named "kidtube"
 mongoose.connect('mongodb://localhost/newsgate');
@@ -12,6 +13,7 @@ require('./config/routes.js')(app, express);
 
 // start listening to requests on port 8000
 app.listen(port);
+app.get('/googleTrends', googleTrends.getGoogleTrends);
 
 // export our app for testing and flexibility, required by index.js
 module.exports = app;
