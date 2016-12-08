5eda8bbee57b28f3c6acc58a323b474c0e1a111a
davidank <dandahmann@gmail.com>
2016-12-08 11:52:44 -0800

    Add working d3 graph example for testing

---
+++ client/app/test/readme.md
@@ -0,0 +1,3 @@
+This is a simple line graph written with d3.js v4 and based on @mbostock's example [here](http://bl.ocks.org/mbostock/02d893e3486c70c4475f). 
+
+This simple graph is designed to be used as a starting point for further development as part of documenting an update to the book [D3 Tips and Tricks](https://leanpub.com/d3-t-and-t-v4) to version 4 of d3.js.
--- client/app/test/test.js
+++ client/app/test/test.js
@@ -4,27 +4,65 @@ angular.module('newsgate.test', [])
   return {
     restrict: 'E',
     link: function(scope, element) {
-      var width = 500, height = 500;
-      var color = d3.scaleOrdinal(d3.schemeCategory10);
-      var pie = d3.pie().sort(null);
-      var arc = d3.arc()
-        .outerRadius(width / 2 * 0.9)
-        .innerRadius(width / 2 * 0.5);
-      // var svg = d3.select(element[0]).append('svg')
-      //   .attrs({width: width, height: height})
-      //   .append('g')
-      //   .attrs('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
-      console.log('element ZERO:', element[0]);
-      var svg = d3.select(element[0]).append('svg');
-      console.log('svg:', svg);
 
 
-      // add the <path>s for each arc slice
-      svg.selectAll('path').data(pie([82, 62, 10, 32]))
-        .enter().append('path')
-        .style('stroke', 'white')
-        .attrs('d', arc)
-        .attrs('fill', function(d, i){ return color(i) });
+      var margin = {top: 20, right: 20, bottom: 30, left: 50},
+          width = 960 - margin.left - margin.right,
+          height = 500 - margin.top - margin.bottom;
+
+      // parse the date / time
+      var parseTime = d3.timeParse("%d-%b-%y");
+
+      // set the ranges
+      var x = d3.scaleTime().range([0, width]);
+      var y = d3.scaleLinear().range([height, 0]);
+
+      // define the line
+      var valueline = d3.line()
+          .x(function(d) { return x(d.date); })
+          .y(function(d) { return y(d.close); });
+
+      // append the svg obgect to the body of the page
+      // appends a 'group' element to 'svg'
+      // moves the 'group' element to the top left margin
+      var svg = d3.select("donut-chart").append("svg")
+          .attr("width", width + margin.left + margin.right)
+          .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+          .attr("transform",
+                "translate(" + margin.left + "," + margin.top + ")");
+
+      // Get the data
+      d3.csv("/app/test/data.csv", function(error, data) {
+        if (error) throw error;
+
+        // format the data
+        data.forEach(function(d) {
+            d.date = parseTime(d.date);
+            d.close = +d.close;
+        });
+
+        // Scale the range of the data
+        x.domain(d3.extent(data, function(d) { return d.date; }));
+        y.domain([0, d3.max(data, function(d) { return d.close; })]);
+
+        // Add the valueline path.
+        svg.append("path")
+            .data([data])
+            .attr("class", "line")
+            .attr("d", valueline);
+
+        // Add the X Axis
+        svg.append("g")
+            .attr("transform", "translate(0," + height + ")")
+            .call(d3.axisBottom(x));
+
+        // Add the Y Axis
+        svg.append("g")
+            .call(d3.axisLeft(y));
+
+      });
+
     }
   };
 });
