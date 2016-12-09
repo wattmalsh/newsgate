0aeb2909c00c6d9a8efc5cf851582b4276d65a2e
FMurray <FMurray@gmail.com>
2016-12-08 16:34:30 -0800

    added CORS middleware

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,7 +1,16 @@
 var bodyParser = require('body-parser');
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
 module.exports = function (app, express) {
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
+  app.use(allowCrossDomain);
   app.use(express.static(__dirname + './../../client'));
 };
