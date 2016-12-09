32fc59198aa0420ae335fabfc712fb4c50461ef4
Aaron Trank <aarontrank@gmail.com>
2016-12-08 16:33:55 -0800

    Robust URL parsing in news route controller

--- server/controllers/newsController.js
+++ server/controllers/newsController.js
@@ -1,6 +1,5 @@
 var News = require('../models/newsModel.js');
 var url = require('url');
-// var testData = require('../test/loadTestData.js');
 
 var sendJSONresponse = function (res, status, content) {
 	res.status(status);
@@ -9,35 +8,43 @@ var sendJSONresponse = function (res, status, content) {
 
 module.exports = {
   isFakeNews: function (req, res, next) {
-    // parse domain from url
-    // just trims
-    var domain = req.body.url.slice(4);
-  	domain = domain.split('/')[0];
-
-  	if (domain) {
-	  	News
-	  		.find({ url: domain})
-	  		.exec(function (err, url) {
-	  			if (!url) {
-	  				sendJSONresponse(res, 404, {
-	  					"message": "url not found"
-	  				});
-	  				return;
-	  			} else if (err) {
-	  				sendJSONresponse(res, 404, err);
-	  				return;
-	  			}
-	  			//sendJSONresponse(res, 200, url);
+		if (req.body.url) {
+			var domain = req.body.url.replace(/^https?:\/\//,''); // Strip off https:// and/or http://
+			domain = domain.replace(/^www\./,''); // Strip off www.
+			domain = domain.split('/')[0]; // Get the domain and just the domain (not the path)
+			if (domain) {
+				News
+				.find({ url: domain})
+				.exec(function (err, url) {
+					if (err) {
+						sendJSONresponse(res, 500, err);
+						return;
+					} else if (url.length === 0) {
+						url.push( {
+							url: domain,
+							rating: {
+									"score" : 0,
+									"type" : "ok",
+									"algorithm" : "v0"
+							}
+						});
+					}
+					//sendJSONresponse(res, 200, url);
 					res.compoundContent = {
 						url: url[0]
 					};
 					next();
-	  		});
-  	} else {
-  		console.log('No url specified');
-  		sendJSONresponse(res, 404, {
-  			"message": "no url in request"
-  		});
-  	}
+				});
+			} else {
+				console.log('No url specified');
+				sendJSONresponse(res, 404, {
+					"message": "Malformed url in request"
+				});
+			}
+		} else {
+			sendJSONresponse(res, 400, { "message": "no url in request" });
+			return;
+		}
+
   }
 };
