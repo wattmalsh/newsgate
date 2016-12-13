9d088a5a4b374ae9cbc3b28cd0ffcd1722dd7605
jamesgu <jamesgu521@gmail.com>
2016-12-13 14:16:55 -0800

    Begin structure of file system

--- ext-test/models/checkForFakes.js
+++ ext-test/models/checkForFakes.js
@@ -2,8 +2,8 @@ function checkForFakes() {
   $('a[href]').each(function(index, element) {
     
     // gets the href value of each element
-    $(element).attr('href')
-
+    // $(element).attr('href')
+    $(element).css('background-color', 'red');
     
     // check db for a match
 
