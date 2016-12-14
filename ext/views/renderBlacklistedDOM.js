dc4677c405219f596370dc582361cd55b5c289ce
jamesgu <jamesgu521@gmail.com>
2016-12-13 19:31:06 -0800

    Test content script and background script interacton

--- ext/views/renderBlacklistedDOM.js
+++ ext/views/renderBlacklistedDOM.js
@@ -1,19 +1,40 @@
-var checkForFakes = function() {
-  var sites = [];
+var renderBlacklistedDOM = function() {
 
-  // gets the href value of each element
-  $('a[href]').each(function(index, element) {
-    sites.push($(element).attr('href'));
-  });
-    
-  // send checkForFakes
-  chrome.runtime.sendMessage({data: 'sites'}, function(response) {
-    console.log(response, '....RESPONSE ON CONTEXT SCRIPT');
-  });
-
-  // if match in blacklist then turn element red
-  // $(element).css('background-color', 'red');
 }
 
-checkForFakes();
 
+// chrome.runtime.sendMessage({data: 'sites'}, function(response) {
+//   console.log(response, '....RESPONSE ON CONTEXT SCRIPT');
+// });
+// var links = $('a[href]');
+// console.log(links);
+// console.log(links.toArray());
+
+var sitesToCompare = $('a[href]');
+var sites = [];
+
+sitesToCompare.each(function(index, element) {
+  sites.push($(element).attr('href'));
+});
+
+chrome.runtime.sendMessage({data: sites}, function(response) {
+
+  // console.log(response.farewell, response.test, '....RESPONSE ON CONTEXT SCRIPT');
+  sitesToCompare.each(function(index, element) {
+    var href = $(element).attr('href');
+    if (response.data.indexOf(href) !== 0) {
+      $(element).css('background-color', 'red');
+    }
+  });
+});
+
+  // console.log($(JSON.parse(response.farewell)));
+  // console.log($(response.farewell) instanceof jQuery);
+  // console.log('JSON STRINGIFIED...', JSON.stringify(response.farewell));
+  // console.log('JSON STRINGIFIED JQUERY', JSON.stringify($(response.farewell)));
+
+
+  // console.log(sites);
+  // .each(function(index, element) {
+  //   sites.push($(element).attr('href'));
+  // });
