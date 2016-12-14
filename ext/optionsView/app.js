ac469eeed68634c1d37b02c356a42eb1dc172db4
Evan Chang <evanc12103832@ucla.edu>
2016-12-14 15:08:55 -0800

    Settings angular base structure

---
+++ ext/optionsView/app.js
@@ -0,0 +1,17 @@
+angular.module('options', [
+    'options.services',
+    'options.blacklist',
+    'options.themes',
+    'ngRoute'])
+  .config(function($routeProvider, $httpProvider) {
+    $routeProvider
+      .when('/themes', {
+        templateUrl: '../optionsView/themes/themes.html',
+        controller: 'themesController'
+      })
+      .when('/blacklist', {
+        templateUrl: '../optionsView/blacklist/blacklist.html',
+        controller: 'blacklistController'
+      })
+      .otherwise({redirectTo: '/themes'});
+  });
