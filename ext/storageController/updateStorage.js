279bc092db69142e924182a6bb1aa117b255df6d
David Wayman <mrdavidwayman@gmail.com>
2016-12-13 20:37:02 -0800

    Abastracts functions to update blacklist

--- ext/storageController/updateStorage.js
+++ ext/storageController/updateStorage.js
@@ -9,19 +9,21 @@
 }
 */
 
-////////////////////////////////////////////////////////////////////////////////
-// UPDATE BLACK LIST: (updates the current persisted blacklist)
-// @input: An array of new URL objects to append to current black list
-////////////////////////////////////////////////////////////////////////////////
-var updateBlacklist = function(newURLs) {
-  var combineBlackList = function(newURLs, currentURLs) {
-    var newBlackList = currentURLs.concat(newURLs);
-    chrome.storage.local.set({ 'blackListedURLs': newBlackList }, function() {
-      alert('Successfully updated black list!');
-    });
-  };
+// Abstracted helper function
+var combineBlackList = function(newURLs, currentURLs, blackListToUpdate) {
+  var newBlackList = currentURLs.concat(newURLs);
+  chrome.storage.local.set({ blackListToUpdate: newBlackList }, function() {
+    alert('Successfully updated black list!');
+  });
+};
 
-  chrome.storage.local.get('blackListedURLs', function(currentURLs) {
-    updateBlacklist(newURLs, currentURLs);
+////////////////////////////////////////////////////////////////////////////////
+// UPDATE BLACK LIST:
+// @input1: An array of new URL objects to append to current black list
+// @input2: Name of blackList to be updated ('blackListedURLs' or 'userGeneratedBlacklist')
+////////////////////////////////////////////////////////////////////////////////
+var updateBlacklist = function(newURLs, blackListToUpdate) {
+  chrome.storage.local.get(nameOfBlacklist, function(currentURLs) {
+    combineBlackList(newURLs, currentURLs, blackListToUpdate);
   });
 };
