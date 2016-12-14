65a04a4cffcd799bf5b67298ec855c3c1229a1a1
jamesgu <jamesgu521@gmail.com>
2016-12-13 20:07:03 -0800

    Continue work on clientside filtering of DOM v. blackslist links

--- ext/views/renderBlacklistedDOM.js
+++ ext/views/renderBlacklistedDOM.js
@@ -2,39 +2,31 @@ var renderBlacklistedDOM = function() {
 
 }
 
-
-// chrome.runtime.sendMessage({data: 'sites'}, function(response) {
-//   console.log(response, '....RESPONSE ON CONTEXT SCRIPT');
-// });
-// var links = $('a[href]');
-// console.log(links);
-// console.log(links.toArray());
-
-var sitesToCompare = $('a[href]');
+var DOMLinks = $('a[href]');
 var sites = [];
 
-sitesToCompare.each(function(index, element) {
-  sites.push($(element).attr('href'));
+var filterLinks = function(unfilteredLink) {
+  var domain = unfilteredLink.replace(/^https?:\/\//,''); // Strip off https:// and/or http://
+  domain = domain.replace(/^www\./,''); // Strip off www.
+  domain = domain.split('/')[0]; // Get the domain and just the domain (not the path)  
+  return domain;
+};
+
+DOMLinks.each(function(index, element) {
+  var href = $(element).attr('href');
+  sites.push(filterLinks(href));
 });
 
 chrome.runtime.sendMessage({data: sites}, function(response) {
 
-  // console.log(response.farewell, response.test, '....RESPONSE ON CONTEXT SCRIPT');
-  sitesToCompare.each(function(index, element) {
+  // loop through DOM links to compare with blacklist returned by ctrlr
+  DOMLinks.each(function(index, element) {
     var href = $(element).attr('href');
-    if (response.data.indexOf(href) !== 0) {
+    var domain = filterLinks(href);
+    console.log('domain');
+    // if link is in blacklist, change css
+    if (response.data.indexOf(domain) !== -1) {
       $(element).css('background-color', 'red');
     }
   });
 });
-
-  // console.log($(JSON.parse(response.farewell)));
-  // console.log($(response.farewell) instanceof jQuery);
-  // console.log('JSON STRINGIFIED...', JSON.stringify(response.farewell));
-  // console.log('JSON STRINGIFIED JQUERY', JSON.stringify($(response.farewell)));
-
-
-  // console.log(sites);
-  // .each(function(index, element) {
-  //   sites.push($(element).attr('href'));
-  // });
