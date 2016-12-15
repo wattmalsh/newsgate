102fec4063458a61dfa73b78c92158aa57de599e
David Wayman <mrdavidwayman@gmail.com>
2016-12-14 18:34:19 -0800

    Tests saving theme sync state

--- ext/optionsView/themes/themes.js
+++ ext/optionsView/themes/themes.js
@@ -1,5 +1,22 @@
 themes = angular.module('options.themes', []);
 
-themes.controller('themesController', function() {
+themes.controller('themesController', function($scope) {
+  $scope.defaultTheme = {
+    theme: {
+      default: 'stuff'
+    }
+  };
 
+  $scope.censorTheme = {
+    theme: {
+      censor: 'more stuff'
+    }
+  }
+
+  $scope.setTheme = function(theme) {
+    console.log('GOT HERE', theme);
+    chrome.storage.sync.set(theme, function() {
+      console.log('Theme successfully saved');
+    });
+  };
 });
