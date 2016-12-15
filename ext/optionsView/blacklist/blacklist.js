d2d49c7d88b5edd99177c86bfda2f4fce50468c4
David Wayman <mrdavidwayman@gmail.com>
2016-12-15 12:05:32 -0800

    Populates user generated blacklist in settings page from sync storage

--- ext/optionsView/blacklist/blacklist.js
+++ ext/optionsView/blacklist/blacklist.js
@@ -1,5 +1,15 @@
 blacklist = angular.module('options.blacklist', []);
 
-blacklist.controller('blacklistController', function() {
+blacklist.controller('blacklistController', function($scope) {
 
+  // GET USER BLACK LIST URLS FROM SYNC STORAGE
+  // then update textarea field on blacklist.html
+  chrome.storage.sync.get('userGeneratedBlacklist', function(syncStore) {
+    var userBlacklistArray = syncStore['userGeneratedBlacklist'];
+    $scope.userBlacklistURLs = userBlacklistArray.join('\n');
+  });
+  
+  // $scope.userBlacklistURLs = "lolzcats\nlolzcats2";
+  // NEED TO GET WHITE LIST URLS FROM SYNC STORAGE
+  $scope.whiteListedURLs = "helloworld.com\nhellothere.biz";
 });
