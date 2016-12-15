1324375f5281c2760c3178bdd66b058c7209d279
David Wayman <mrdavidwayman@gmail.com>
2016-12-14 19:08:31 -0800

    Saves themes to sync storage, updates css based on theme in renderBlacklistedDOM.js

--- ext/optionsView/themes/themes.js
+++ ext/optionsView/themes/themes.js
@@ -3,18 +3,18 @@ themes = angular.module('options.themes', []);
 themes.controller('themesController', function($scope) {
   $scope.defaultTheme = {
     theme: {
-      default: 'stuff'
+      'background-color': 'red'
     }
   };
 
   $scope.censorTheme = {
     theme: {
-      censor: 'more stuff'
+      'background-color': 'black',
+      'color': 'black'
     }
   }
 
   $scope.setTheme = function(theme) {
-    console.log('GOT HERE', theme);
     chrome.storage.sync.set(theme, function() {
       console.log('Theme successfully saved');
     });
