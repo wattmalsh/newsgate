dc2b8a9ffc1c294aaad13557fe4d3323af3b3d1f
Evan Chang <evanc12103832@ucla.edu>
2016-12-15 16:27:19 -0800

    contextMenu infrastructure

--- ext/models/contextMenu.js
+++ ext/models/contextMenu.js
@@ -1,11 +1,24 @@
 
 var clickHandler = function(word) {
-  console.log(this);
-  if (filterLinks(word.linkUrl) === 'google.com') {
-    var domain = filterGoogleDomain(word.linkUrl);
+  var domain = filterLinks(word.linkUrl) === 'google.com' ? filterGoogleDomain(word.linkUrl) : filterLinks(word.linkUrl);
+  if (_.contains(whitelist, domain)) {
+      chrome.contextMenus.update(menu, {"title": "Remove from Whitelist", "onclick": removeFromWhitelist});
   } else {
-    var domain = filterLinks(word.linkUrl);
+    chrome.contextMenus.update(menu, {"onclick": addToWhitelist});
   }
+};
+
+var removeFromWhitelist = function(word) {
+  var domain = filterLinks(word.linkUrl) === 'google.com' ? filterGoogleDomain(word.linkUrl) : filterLinks(word.linkUrl);
+  getWhitelist(function(whitelist) {
+    setWhitelistTo(_.filter(whitelist, function(item) {
+      return item !== domain;
+    })/* optional callback here*/);
+  });
+};
+
+var addToWhitelist = function(word) {
+  var domain = filterLinks(word.linkUrl) === 'google.com' ? filterGoogleDomain(word.linkUrl) : filterLinks(word.linkUrl);
 };
 
 var filterGoogleDomain = function(unfilteredLink) {
@@ -18,7 +31,8 @@ var filterGoogleDomain = function(unfilteredLink) {
 chrome.runtime.onInstalled.addListener(function() {
   var context = "link";
   var title = "Add to Whitelist";
-  chrome.contextMenus.create({
+  var menu = chrome.contextMenus.create({
+    "id":
     "title": title,
     "contexts": [context],
     "onclick" : clickHandler
