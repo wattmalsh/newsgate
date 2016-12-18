c1829756d16a1f1a248877af0c0af935bb760acf
jamesgu <jamesgu521@gmail.com>
2016-12-18 01:58:27 -0800

    Finish basic disable functionality

--- ext/disableController/disable.js
+++ ext/disableController/disable.js
@@ -1,22 +1,3 @@
-// var setAlarm = function(name, ms) {
-//   if (chrome.alarms) {
-//     chrome.alarms.clearAll(function() {
-//       createAlarm(name, ms);
-//     })
-//   } else {
-//     console.log('in creating')
-//     createAlarm(name, ms);
-//   }
-// };
-
-// function alarmListener(alarm) {
-//   if (alarm === 'disableAlarm') {
-//     console.log(alarm);
-//     console.log('inside');
-//     chrome.alarms.clear('disableAlarm');
-//   }
-// }
-
 var setAlarm = function(name, min) {
   // Create alarm and ensure there are no other outstanding alarms
   chrome.alarms.clearAll(function() {
@@ -26,9 +7,7 @@ var setAlarm = function(name, min) {
 
 // Add listener to alarm
 chrome.alarms.onAlarm.addListener(function alarmListener(alarm) {
-  console.log(alarm, 'alarm props');
   if (alarm.name === 'disableAlarm') {
-    console.log('correct alarm');
     setDisabledState(false, function() {
       console.log('Reverted back to enabled');
     });
--- ext/optionsView/general/general.js
+++ ext/optionsView/general/general.js
@@ -2,12 +2,11 @@ general = angular.module('options.general', []);
 
 general.controller('generalController', function($scope, General) {
 
-  // Automatically sets radio to ngvalue
-  $scope.minutes = 60;
-
   // Watch for changes in model
   $scope.$watch('minutes', function(minutes) {
     console.log('disable in controller called', minutes);
-    General.disable(minutes);
+    if (minutes !== undefined) {
+      General.disable(minutes);
+    }
   })
 });
