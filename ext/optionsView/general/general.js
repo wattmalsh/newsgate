58047b78505380f7aa070bae7ae8f2722aad0199
jamesgu <jamesgu521@gmail.com>
2016-12-17 22:23:19 -0800

    Set up angular for disable functionality

--- ext/optionsView/general/general.js
+++ ext/optionsView/general/general.js
@@ -1,5 +1,11 @@
 general = angular.module('options.general', []);
 
-general.controller('generalController', function() {
+general.controller('generalController', function(General) {
+
+  // Function for 10 min disable
+
+  // Function for 30 min disable
+
+  // Function for 60 min disable
 
 });
--- ext/optionsView/services.js
+++ ext/optionsView/services.js
@@ -12,5 +12,14 @@ services.factory('Themes', function() {
 
 services.factory('General', function() {
 
+  // Input minutes need app to be disabled
+  var disable = function(min) {
+
+    // Get current state from sync storage
+
+    // Set disabled to true
+
+    // Use set timeout to toggle back to true
+  }
 
 });
